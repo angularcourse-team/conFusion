a957a02e96724e02203acc0aaa0723f64eff85d8
Ron Deen <rjp.deen@gmail.com>
2019-06-05 07:07:48 +0200

    Configuring Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import 'hammerjs';
+
 @NgModule({
   declarations: [
     AppComponent
