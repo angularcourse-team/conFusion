e5a014ff635632f6c1cbf382877d1a2381c39159
Ron Deen <rjp.deen@gmail.com>
2019-06-17 23:04:02 +0200

    Single Page app part1

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Dish } from '../shared/dish';
-import { DISHES} from '../shared/dishes';
+import { DISHES } from '../shared/dishes';
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +11,13 @@ export class DishService {
 
   getDishes(): Dish[] {
     return DISHES;
+  }
+
+  getDish(id: string): Dish {
+    return DISHES.filter((dish) => (dish.id === id) )[0];
+  }
+
+  getFeaturedDish(): Dish {
+    return DISHES.filter((dish) => dish.featured)[0];
   }
 }
